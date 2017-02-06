e7163ad8583d15818b4c8a8b9b5ad165887d1493
Emil Kirilov <emil.kirilovk@gmail.com>
2017-02-06 20:32:46 +0200

    and vs &&

---
+++ and vs &&.rb
@@ -0,0 +1,22 @@
+foo = :foo
+bar = nil
+
+a = foo and bar
+# => nil
+a
+# => :foo
+
+a = foo && bar
+# => nil
+a
+# => nil
+
+a = (foo and bar)
+# => nil
+a
+# => nil
+
+(a = foo) && bar
+# => nil
+a
+# => :foo
