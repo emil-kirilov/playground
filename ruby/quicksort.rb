e4ce3b5dd41c5637b2772c6ae5b9a33808f34f82
Emil Kirilov <emil.kirilovk@gmail.com>
2017-03-05 23:11:13 +0200

    quicksort on 3 rows

--- ruby/quicksort.rb
+++ ruby/quicksort.rb
@@ -1,8 +1,7 @@
 def quicksort list
 	return [] if list.empty?
-	x, *xs = *list
-	smallerOrEqual, bigger = xs.partition { |el| el <= x }
-	quicksort(smallerOrEqual) + [x] + quicksort(bigger)
+	smallerOrEqual, bigger = list.drop(1).partition { |el| el <= list.first }
+	quicksort(smallerOrEqual) + [list.first] + quicksort(bigger)
 end
 
 p quicksort [1,2,3345,54,234,123,2,-23,4234,-324,2134]
