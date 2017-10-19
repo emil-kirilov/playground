27d7a2e01725111e9fc81e85d9ab45e33704e905
Emil Kirilov <emil.kirilovk@gmail.com>
2017-10-19 12:59:13 +0300

    Update quicksort.rb

--- ruby/quicksort.rb
+++ ruby/quicksort.rb
@@ -1,7 +1,7 @@
-def quicksort list
-	return [] if list.empty?
-	smallerOrEqual, bigger = list.drop(1).partition { |el| el <= list.first }
-	quicksort(smallerOrEqual) + [list.first] + quicksort(bigger)
+def quicksort(arr)
+	return [] if arr.empty?
+	smaller_or_equal, bigger = arr.drop(1).partition { |el| el <= arr.first }
+	quicksort(smaller_or_equal) + [arr.first] + quicksort(bigger)
 end
 
 p quicksort [1,2,3345,54,234,123,2,-23,4234,-324,2134]
